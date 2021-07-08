c67760a6b151a02c7bfbfa3d0b19259e1d428958
endol007 <37258336+endol007@users.noreply.github.com>
2021-07-08 18:22:16 +0900

    좋아요 버튼클릭시 애니메이션 반영

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,14 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import {useSpring, animated} from "react-spring";
 
 import heart_black from "../shared/BlackHeart.png";
 import heart_red from "../shared/RedHeart.png";
 
 
 const Like = (props) => {
+    const styles = useSpring({
+        to:[{opacity: 1,
+            width: "100%",
+            height:"100%",
+            position: "absolute",
+            },
+            {opacity : 0 }],
+        from: {opacity: 0}
+    })
+
     const icon = props.is_like==true? heart_red : heart_black;
         return (
+            <React.Fragment>
+                {props.is_like==true? (<animated.div style={styles}><Image src={heart_red}></Image></animated.div>)
+                : (<div></div>) }
+                
             <LikeBtn icon={icon} onClick={props._onClick} ></LikeBtn>
+            </React.Fragment>
     )
 }
 
@@ -19,6 +35,9 @@ const LikeBtn = styled.div`
     background-image: url(${(props) => props.icon});
     background-size: cover;
     cursor: pointer;
+`;
+const Image = styled.img`
+    position: absolute;
 `;
 
 
